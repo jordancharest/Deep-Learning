d9dcc3ff0a6b3c665a929666ab338bef317b459e
Jordan <jordancharest3@gmail.com>
2018-01-07 19:42:59 -0500

    Started building an artificial neural network with Keras

---
+++ Artificial-Neural-Networks/ann.py
@@ -0,0 +1,73 @@
+# Artificial Neural Network
+
+
+# Data Preprocessing
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import the dataset
+dataset = pd.read_csv('Churn_Modeling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encode categorical data (country and gender)
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_country = LabelEncoder()
+X[:, 1] = labelencoder_country.fit_transform(X[:, 1])
+labelencoder_gender = LabelEncoder()
+X[:, 2] = labelencoder_gender.fit_transform(X[:, 2])
+
+# Split Country data into different columns
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoid Dummy Variable Trap
+X = X[:, 1:]
+
+# Split the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+# Make the Neural Network
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialize the ANN
+classifier = Sequential()
+
+# Add the input layer and the first hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+
+# Add the second hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+
+# Add the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compile the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fit the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+
+
+# Make predictions and evaluating the model
+
+# Predict the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+y_pred = (y_pred > 0.5)
+cm = confusion_matrix(y_test, y_pred)
