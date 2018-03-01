8f0268ec3f5edc5f65528f9af0ef67e7557970f0
Jordan <jordancharest3@gmail.com>
2018-03-01 12:37:47 -0500

    Made predictions and plotted against real output

--- Recurrent-Neural-Networks/rnn.py
+++ Recurrent-Neural-Networks/rnn.py
@@ -21,10 +21,8 @@ output_train = []
 timesteps = 60
 
 for i in range(timesteps, len(training_set_scaled) ):
-    # contains Open stock price from the previous timesteps
-    input_train.append(training_set_scaled[i-timesteps:i, 0])
-    # contains current Open stock price
-    output_train.append(training_set_scaled[i, 0])
+    input_train.append(training_set_scaled[i-timesteps:i, 0])   # contains Open stock price from the previous timesteps
+    output_train.append(training_set_scaled[i, 0])  # contains current Open stock price
     
 input_train = np.array(input_train)
 output_train = np.array(output_train)
@@ -65,9 +63,40 @@ regressor.add(Dropout(0.2))
 # Add Output Layer
 regressor.add(Dense(units=1))
 
-
+# Compile
 regressor.compile(optimizer='adam', loss='mean_squared_error')  # RMSprop is usually a good choice for RNNs (adam is used here on suggestion from Hadelin)
 
 
 # Fit RNN to the training set
 regressor.fit(input_train, output_train, epochs=100, batch_size=32) # batch_size = how many input data points before back propagation
+
+
+## MAKING PREDICTIONS =============================================================================
+raw_test_data = pd.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price = raw_test_data.iloc[:, 1:2].values
+
+# Predict the stock price
+total_dataset = pd.concat((raw_training_data['Open'], raw_test_data['Open']), axis=0)   # verical concatenation
+inputs = total_dataset[len(total_dataset)-len(raw_test_data)-60: ].values
+inputs = inputs.reshape(-1,1)
+inputs = scale.transform(inputs)
+
+input_test = []
+
+for i in range(timesteps, len(raw_test_data)+timesteps ):
+    input_test.append(inputs[i-timesteps:i, 0])   # contains Open stock price from the previous timesteps
+        
+input_test = np.array(input_test)
+input_test = np.reshape(input_test, (input_test.shape[0], timesteps, 1))  # (batch_size, timesteps, input_dims)
+
+predicted_stock_price = regressor.predict(input_test)
+predicted_stock_price = scale.inverse_transform(predicted_stock_price)
+
+# Visualize the results
+plt.plot(real_stock_price, color='red', label = 'Real Stock Price')
+plt.plot(predicted_stock_price, color='blue', label = 'Predicted Stock Price')
+plt.title('Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Stock Price')
+plt.legend()
+plt.show()
